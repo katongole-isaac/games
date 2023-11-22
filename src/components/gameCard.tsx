677b74c6b213ab9451d28d1e04c038aea2bc764c
katongole isaac <katongolelsaac78@gmail.com>
2023-11-22 13:27:36 +0300

    Removed duplicate styles

--- src/components/gameCard.tsx
+++ src/components/gameCard.tsx
@@ -8,9 +8,10 @@ import getCroppedImage from "../services/imageUrl";
 interface Props {
   game: Game;
 }
+
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card >
       <Image src={ getCroppedImage(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl"> {game.name} </Heading>
--- src/components/gameGrid.tsx
+++ src/components/gameGrid.tsx
@@ -4,6 +4,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./gameCard";
 import GameCardSkeleton from "./gameCardSkeleton";
+import GameCardContainer from "./gameCardContainer";
 
 const GameGrid = () => {
 
@@ -14,15 +15,25 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text> {error.message} </Text>}
-      <SimpleGrid columns={{ sm: 1 , md: 2, lg: 3, xl :4 }} paddingX="10px" spacing={10} >
-     {
-      isLoading && Array(numberOfSkeletons).fill(null)
-        .map( (_, index)=> <GameCardSkeleton key={index} />)
-     }
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        paddingX="10px"
+        spacing={10}
+      >
+        {isLoading &&
+          Array(numberOfSkeletons)
+            .fill(null)
+            .map((_, index) => (
+              <GameCardContainer key={index} >
+                <GameCardSkeleton  />
+              </GameCardContainer>
+            ))}
         {games?.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
+          </GameCardContainer>
         ))}
-      </SimpleGrid >
+      </SimpleGrid>
     </>
   );
 };
