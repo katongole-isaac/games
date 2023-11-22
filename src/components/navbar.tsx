2e4da2ccce0ae5577ae936db09ec13361fd7a594
katongole isaac <katongolelsaac78@gmail.com>
2023-11-22 19:27:03 +0300

    Add logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,23 @@
-import { Box, HStack, Text } from "@chakra-ui/react";
+import { Box, HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./colorModeSwitch";
 import SearchInput from "./searchInput";
 import { GameContext, GameQueryContext } from "../App";
 import { useContext } from "react";
+import Logo from "../assets/game_logo.png";
 
 const Navbar = () => {
-
   const { onSearch } = useContext(GameQueryContext) as GameContext;
 
   return (
-    <HStack justifyContent="space-between" paddingX="10px" py={5} >  
+    <HStack justifyContent="space-between" paddingX="10px" py={5}>
       <Box>
-        <Text> Logo </Text>
+        <Box width="100px"  >
+          <Image src={Logo} />
+        </Box>
       </Box>
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
-    </HStack> 
+    </HStack>
   );
 };
 
