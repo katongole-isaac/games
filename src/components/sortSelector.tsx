a5e07e5e4644879fd26c66bc20efa52079dd07a9
katongole isaac <katongolelsaac78@gmail.com>
2023-11-22 17:53:51 +0300

    Build sort selector

---
+++ src/components/sortSelector.tsx
@@ -0,0 +1,21 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+
+const SortSelector = () => {
+  return (
+    <Menu>
+      <MenuButton mb={5} as={Button} rightIcon={<BsChevronDown />}>
+        Order by: Relevance
+      </MenuButton>
+      <MenuList>
+        <MenuItem>Relevance</MenuItem>
+        <MenuItem>Date Added</MenuItem>
+        <MenuItem>Release Date</MenuItem>
+        <MenuItem>Name</MenuItem>
+        <MenuItem>Rating</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
