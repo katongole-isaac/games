99680ac0ce6853722ff80803153df160974485a1
katongole isaac <katongolelsaac78@gmail.com>
2023-11-22 13:03:02 +0300

    Getting optimized Images

---
+++ src/components/criticScore.tsx
@@ -0,0 +1,17 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+const CriticScore = ({ score }: Props) => {
+
+  let color = score > 75 ? "green" : score > 65 ? "yellow" : "";
+
+  return (
+    <Badge colorScheme={color} fontSize="16px" borderRadius="4px" paddingX={2}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
--- src/components/gameCard.tsx
+++ src/components/gameCard.tsx
@@ -1,18 +1,25 @@
-import { Card, CardBody, Heading, Image, } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./platformIconList";
+import CriticScore from "./criticScore";
+import getCroppedImage from "../services/imageUrl";
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden" >
-      <Image src={game.background_image} />
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={ getCroppedImage(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl" > {game.name} </Heading>
-        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+        <Heading fontSize="2xl"> {game.name} </Heading>
+        <HStack justifyContent="space-between" >
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
